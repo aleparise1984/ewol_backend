3b3500c9f81fa339ed303071f8f8f41757fcc378
aleparise1984 <106113257+aleparise1984@users.noreply.github.com>
2023-12-01 21:54:01 -0300

    fix cors 2

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,21 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from "@nestjs/core";
 
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
   const config = new DocumentBuilder()
-    .setTitle('API')
-    .setDescription('ewolAcademy')
-    .setVersion('1.0')
+    .setTitle("API")
+    .setDescription("ewolAcademy")
+    .setVersion("1.0")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup("docs", app, document);
 
   app.enableCors({
     origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept',
-    credentials: true,
   });
   await app.listen(process.env.PORT || 3001);
 }
