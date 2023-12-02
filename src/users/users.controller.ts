ae6e8c78de4fb9cd0fdd313744c9707be114bcd2
aleparise1984 <106113257+aleparise1984@users.noreply.github.com>
2023-12-01 21:30:24 -0300

    fix findAll

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,20 +8,20 @@ import {
   UseGuards,
   Patch,
   Request,
-} from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto, UpdateUserDto } from './dto/users.dto';
+} from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { CreateUserDto, UpdateUserDto } from "./dto/users.dto";
 
-import { Public } from './../auth/decorators/public.decorator';
-import { RolesGuard } from './../auth/guards/roles.guard';
-import { Roles } from './../auth/decorators/roles.decorator';
-import { JwtAuthGuard } from './../auth/guards/jwt-auth.guard';
-import { Role } from './../auth/models/roles.model';
-import { AuthGuard } from '@nestjs/passport';
-import { User } from './entities/users.entity';
+import { Public } from "./../auth/decorators/public.decorator";
+import { RolesGuard } from "./../auth/guards/roles.guard";
+import { Roles } from "./../auth/decorators/roles.decorator";
+import { JwtAuthGuard } from "./../auth/guards/jwt-auth.guard";
+import { Role } from "./../auth/models/roles.model";
+import { AuthGuard } from "@nestjs/passport";
+import { User } from "./entities/users.entity";
 
 @UseGuards(JwtAuthGuard, RolesGuard)
-@Controller('users')
+@Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
@@ -32,33 +32,32 @@ export class UsersController {
   }
 
   @Public()
-  @Post('student')
+  @Post("student")
   createStudent(@Body() createUserDto: CreateUserDto) {
     return this.usersService.createStudent(createUserDto);
   }
 
-  @Public()
+  // @Public()
   @Get()
-  findAll(Request: Request) {
-    console.log(Request.headers);
+  findAll() {
     return this.usersService.findAll();
   }
 
   @Public()
-  @Get('/candidates')
+  @Get("/candidates")
   findCandidates() {
     return this.usersService.findCandidate();
   }
 
   @Public()
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Public()
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 }
