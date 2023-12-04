7671cac40fc09fd8dca21056901125a184af8f0b
aleparise1984 <106113257+aleparise1984@users.noreply.github.com>
2023-12-04 14:39:36 -0300

    update main

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,24 @@
 import { NestFactory } from "@nestjs/core";
-
+import { ValidationPipe } from "@nestjs/common";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: "*",
+  });
+
   const config = new DocumentBuilder()
     .setTitle("API")
-    .setDescription("ewolAcademy")
+    .setDescription("AEPC API description")
     .setVersion("1.0")
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("docs", app, document);
 
-  app.enableCors({
-    origin: '*',
-  });
+  app.enableCors();
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
